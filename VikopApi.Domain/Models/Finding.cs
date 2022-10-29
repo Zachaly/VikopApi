dcebfdfa232e29c669c6831aeaeaf8ec69bef378
Zachary Soja <zachary3003@gmail.com>
2022-10-30 00:18:24 +0200

    tests: add application user manager tests

--- VikopApi.Domain/Models/Finding.cs
+++ VikopApi.Domain/Models/Finding.cs
@@ -3,11 +3,11 @@
     public class Finding
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Link { get; set; }
-        public string Picture { get; set; }
-        public string CreatorId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Link { get; set; }
+        public string? Picture { get; set; }
+        public string? CreatorId { get; set; }
         public ApplicationUser Creator { get; set; }
         public DateTime Created { get; set; }
         public ICollection<FindingComment> Comments { get; set; }
