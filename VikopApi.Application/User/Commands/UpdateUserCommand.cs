e9fb5b4349d1db89278e58b110332c39720c5237
Zachary Soja <zachary3003@gmail.com>
2022-11-05 16:13:18 +0100

    fix: change update user command

--- VikopApi.Application/User/Commands/UpdateUserCommand.cs
+++ VikopApi.Application/User/Commands/UpdateUserCommand.cs
@@ -1,32 +1,37 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using VikopApi.Application.Auth.Abstractions;
+using VikopApi.Application.Command.Abstractions;
 using VikopApi.Application.Files.Abstractions;
+using VikopApi.Application.Models;
 using VikopApi.Application.Models.Requests;
 using VikopApi.Application.User.Abstractions;
 
 namespace VikopApi.Application.User.Commands
 {
-    public class UpdateUserCommand : IRequest
+    public class UpdateUserCommand : IRequest<CommandResponseModel>
     {
         public string Username { get; set; }
         public IFormFile? ProfilePicture { get; set; }
     }
 
-    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand>
+    public class UpdateUserHandler : IRequestHandler<UpdateUserCommand, CommandResponseModel>
     {
         private readonly IAuthService _authService;
         private readonly IFileService _fileService;
         private readonly IUserService _userService;
+        private readonly ICommandResponseFactory _commandResponseFactory;
 
-        public UpdateUserHandler(IAuthService authService, IUserService userService, IFileService fileService)
+        public UpdateUserHandler(IAuthService authService, IUserService userService, IFileService fileService,
+            ICommandResponseFactory commandResponseFactory)
         {
             _authService = authService;
             _fileService = fileService;
             _userService = userService;
+            _commandResponseFactory = commandResponseFactory;
         }
 
-        public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        public async Task<CommandResponseModel> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
             var updateRequest = new UpdateUserRequest
             {
@@ -43,7 +48,7 @@ namespace VikopApi.Application.User.Commands
 
             await _userService.UpdateUser(updateRequest);
 
-            return Unit.Value;
+            return _commandResponseFactory.CreateSuccess();
         }
     }
 }
